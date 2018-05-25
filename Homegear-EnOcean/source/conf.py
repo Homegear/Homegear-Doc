5f2be306692702a403367133816f5f6030eff143
Sathya Laufer <sathya@laufers.net>
2018-05-25 11:29:49 +0200

    Added imprint and privacy policy

--- Homegear-EnOcean/source/conf.py
+++ Homegear-EnOcean/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Homegear EnOcean Manual'
-copyright = u'2017, Sathya Laufer'
+copyright = u'2016-2018, Homegear GmbH'
 author = u'Sathya Laufer'
 
 # The version info for the project you're documenting, acts as replacement for
--- Homegear-HomeMaticBidCoS/source/conf.py
+++ Homegear-HomeMaticBidCoS/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Homegear HomeMatic BidCoS Manual'
-copyright = u'2016, Sathya Laufer'
+copyright = u'2016-2018, Homegear GmbH'
 author = u'Sathya Laufer'
 
 # The version info for the project you're documenting, acts as replacement for
